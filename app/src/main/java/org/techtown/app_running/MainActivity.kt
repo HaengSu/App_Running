ab04ce1823e10371b534daa52df81d798a2287ff
Haeng <onse3840@gmail.com>
2022-02-21 10:39:18 +0900

    commit

---
+++ app/src/main/java/org/techtown/app_running/MainActivity.kt
@@ -0,0 +1,14 @@
+package org.techtown.app_running
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+//Push Test
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
